7c42a7cfd245e66f953cc27d34fc3f5b2c28affc
EKIZAMANI <eki.zamani@gmail.com>
2023-11-15 10:56:05 +0700

    change: table of database

--- database/renthouse.sql
+++ database/renthouse.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 16, 2020 at 02:00 PM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.7
+-- Waktu pembuatan: 10 Nov 2023 pada 06.08
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `add_property`
+-- Struktur dari tabel `add_property`
 --
 
 CREATE TABLE `add_property` (
@@ -48,31 +48,33 @@ CREATE TABLE `add_property` (
   `description` varchar(2000) NOT NULL,
   `latitude` decimal(9,6) NOT NULL,
   `longitude` decimal(9,6) NOT NULL,
-  `owner_id` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `owner_id` int(10) NOT NULL,
+  `booked` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `add_property`
+-- Dumping data untuk tabel `add_property`
 --
 
-INSERT INTO `add_property` (`property_id`, `country`, `province`, `zone`, `district`, `city`, `vdc_municipality`, `ward_no`, `tole`, `contact_no`, `property_type`, `estimated_price`, `total_rooms`, `bedroom`, `living_room`, `kitchen`, `bathroom`, `description`, `latitude`, `longitude`, `owner_id`) VALUES
-(123, 'Nepal', 'Bagmati Pradesh', 'Bagmati', 'Taplejung', 'Kirtipur', 'Municipality', 3, 'palifal', 9860462146, 'Full House Rent', 2000000, 2, 3, 3, 3, 3, 'nmjhvgc', '27.679130', '85.327872', 1),
-(124, 'Nepal', 'Gandaki Pradesh', 'Gandaki', 'Kaski', 'Pokhara', 'Municipality', 14, 'Lakeside', 9803480519, 'Full House Rent', 2000000, 15, 5, 2, 2, 7, 'This is a beautiful property located near Lakeside, Pokhara.', '27.679130', '85.327872', 1);
+INSERT INTO `add_property` (`property_id`, `country`, `province`, `zone`, `district`, `city`, `vdc_municipality`, `ward_no`, `tole`, `contact_no`, `property_type`, `estimated_price`, `total_rooms`, `bedroom`, `living_room`, `kitchen`, `bathroom`, `description`, `latitude`, `longitude`, `owner_id`, `booked`) VALUES
+(123, 'Nepal', 'Bagmati Pradesh', 'Bagmati', 'Taplejung', 'Kirtipur', 'Municipality', 3, 'palifal', 9860462146, 'Full House Rent', 2000000, 2, 3, 3, 3, 3, 'nmjhvgc', 27.679130, 85.327872, 1, ''),
+(124, 'Nepal', 'Gandaki Pradesh', 'Gandaki', 'Kaski', 'Pokhara', 'Municipality', 14, 'Lakeside', 9803480519, 'Full House Rent', 2000000, 15, 5, 2, 2, 7, 'This is a beautiful property located near Lakeside, Pokhara.', 27.679130, 85.327872, 1, ''),
+(125, 'Nepal', 'Province No. 1', 'Bheri', 'Panchthar', 'BANDA ACEH', '', 0, 'asd', 0, 'Full House Rent', 123123123, 1, 1, 1, 1, 1, '1', 5.568923, 95.362562, 2, 'Yes');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Struktur dari tabel `admin`
 --
 
 CREATE TABLE `admin` (
   `admin_id` int(10) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `admin`
+-- Dumping data untuk tabel `admin`
 --
 
 INSERT INTO `admin` (`admin_id`, `email`, `password`) VALUES
@@ -81,7 +83,46 @@ INSERT INTO `admin` (`admin_id`, `email`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `owner`
+-- Struktur dari tabel `booking`
+--
+
+CREATE TABLE `booking` (
+  `tenant_id` int(11) NOT NULL,
+  `property_id` int(11) NOT NULL,
+  `booked` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `booking`
+--
+
+INSERT INTO `booking` (`tenant_id`, `property_id`, `booked`) VALUES
+(18, 125, '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `chat`
+--
+
+CREATE TABLE `chat` (
+  `owner_id` int(255) NOT NULL,
+  `tenant_id` int(255) NOT NULL,
+  `message` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `chat`
+--
+
+INSERT INTO `chat` (`owner_id`, `tenant_id`, `message`) VALUES
+(1, 18, 'hello'),
+(2, 18, 'hai za\r\n');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `owner`
 --
 
 CREATE TABLE `owner` (
@@ -93,41 +134,43 @@ CREATE TABLE `owner` (
   `address` varchar(200) NOT NULL,
   `id_type` varchar(100) NOT NULL,
   `id_photo` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `owner`
+-- Dumping data untuk tabel `owner`
 --
 
 INSERT INTO `owner` (`owner_id`, `full_name`, `email`, `password`, `phone_no`, `address`, `id_type`, `id_photo`) VALUES
-(1, 'Nikesh Tiwari', 'nikeshtiwari3230@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 987654321, 'Kirtipur-3', 'Citizenship', 'owner-photo/nikesh.png');
+(1, 'Nikesh Tiwari', 'nikeshtiwari3230@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 987654321, 'Kirtipur-3', 'Citizenship', 'owner-photo/nikesh.png'),
+(2, 'eki', 'eki@gmail.com', '202cb962ac59075b964b07152d234b70', 123123123, '123', 'Driving Licence', 'owner-photo/WhatsApp Image 2021-08-17 at 16.18.23.jpeg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `property_photo`
+-- Struktur dari tabel `property_photo`
 --
 
 CREATE TABLE `property_photo` (
   `property_photo_id` int(12) NOT NULL,
   `p_photo` varchar(500) NOT NULL,
   `property_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `property_photo`
+-- Dumping data untuk tabel `property_photo`
 --
 
 INSERT INTO `property_photo` (`property_photo_id`, `p_photo`, `property_id`) VALUES
 (174, 'product-photo/a.jpg', 123),
 (175, 'product-photo/b.jpg', 123),
 (176, 'product-photo/b.jpg', 124),
-(177, 'product-photo/a.jpg', 124);
+(177, 'product-photo/a.jpg', 124),
+(178, 'product-photo/Screenshot 2022-11-26 223337.jpg', 125);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `review`
+-- Struktur dari tabel `review`
 --
 
 CREATE TABLE `review` (
@@ -135,20 +178,22 @@ CREATE TABLE `review` (
   `comment` varchar(500) NOT NULL,
   `rating` int(5) NOT NULL,
   `property_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `review`
+-- Dumping data untuk tabel `review`
 --
 
 INSERT INTO `review` (`review_id`, `comment`, `rating`, `property_id`) VALUES
 (5, 'This property is very nice.', 5, 123),
-(6, 'I love this property.', 4, 124);
+(6, 'I love this property.', 4, 124),
+(7, 'ssoo good', 5, 123),
+(8, 'this properties so niceee', 4, 125);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tenant`
+-- Struktur dari tabel `tenant`
 --
 
 CREATE TABLE `tenant` (
@@ -160,116 +205,117 @@ CREATE TABLE `tenant` (
   `address` varchar(200) NOT NULL,
   `id_type` varchar(100) NOT NULL,
   `id_photo` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tenant`
+-- Dumping data untuk tabel `tenant`
 --
 
 INSERT INTO `tenant` (`tenant_id`, `full_name`, `email`, `password`, `phone_no`, `address`, `id_type`, `id_photo`) VALUES
-(17, 'Nikesh Tiwari', 'nikeshtiwari3230@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 987654321, 'Kirtipur-3', 'Citizenship', 'tenant-photo/nikesh.png');
+(17, 'Nikesh Tiwari', 'nikeshtiwari3230@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 987654321, 'Kirtipur-3', 'Citizenship', 'tenant-photo/nikesh.png'),
+(18, 'zaki', 'zaki@gmail.com', '202cb962ac59075b964b07152d234b70', 123123123, 'zaki', 'Citizenship', 'tenant-photo/WhatsApp Image 2021-08-17 at 16.18.23.jpeg');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `add_property`
+-- Indeks untuk tabel `add_property`
 --
 ALTER TABLE `add_property`
   ADD PRIMARY KEY (`property_id`),
   ADD KEY `owner_id` (`owner_id`);
 
 --
--- Indexes for table `admin`
+-- Indeks untuk tabel `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
 
 --
--- Indexes for table `owner`
+-- Indeks untuk tabel `owner`
 --
 ALTER TABLE `owner`
   ADD PRIMARY KEY (`owner_id`);
 
 --
--- Indexes for table `property_photo`
+-- Indeks untuk tabel `property_photo`
 --
 ALTER TABLE `property_photo`
   ADD PRIMARY KEY (`property_photo_id`),
   ADD KEY `property_id` (`property_id`);
 
 --
--- Indexes for table `review`
+-- Indeks untuk tabel `review`
 --
 ALTER TABLE `review`
   ADD PRIMARY KEY (`review_id`),
   ADD KEY `property_id` (`property_id`);
 
 --
--- Indexes for table `tenant`
+-- Indeks untuk tabel `tenant`
 --
 ALTER TABLE `tenant`
   ADD PRIMARY KEY (`tenant_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `add_property`
+-- AUTO_INCREMENT untuk tabel `add_property`
 --
 ALTER TABLE `add_property`
-  MODIFY `property_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
+  MODIFY `property_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=126;
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT untuk tabel `admin`
 --
 ALTER TABLE `admin`
   MODIFY `admin_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `owner`
+-- AUTO_INCREMENT untuk tabel `owner`
 --
 ALTER TABLE `owner`
-  MODIFY `owner_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `owner_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `property_photo`
+-- AUTO_INCREMENT untuk tabel `property_photo`
 --
 ALTER TABLE `property_photo`
-  MODIFY `property_photo_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=178;
+  MODIFY `property_photo_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=179;
 
 --
--- AUTO_INCREMENT for table `review`
+-- AUTO_INCREMENT untuk tabel `review`
 --
 ALTER TABLE `review`
-  MODIFY `review_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `review_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `tenant`
+-- AUTO_INCREMENT untuk tabel `tenant`
 --
 ALTER TABLE `tenant`
-  MODIFY `tenant_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `tenant_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `add_property`
+-- Ketidakleluasaan untuk tabel `add_property`
 --
 ALTER TABLE `add_property`
   ADD CONSTRAINT `add_property_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `owner` (`owner_id`);
 
 --
--- Constraints for table `property_photo`
+-- Ketidakleluasaan untuk tabel `property_photo`
 --
 ALTER TABLE `property_photo`
   ADD CONSTRAINT `property_photo_ibfk_1` FOREIGN KEY (`property_id`) REFERENCES `add_property` (`property_id`);
 
 --
--- Constraints for table `review`
+-- Ketidakleluasaan untuk tabel `review`
 --
 ALTER TABLE `review`
   ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`property_id`) REFERENCES `add_property` (`property_id`);
